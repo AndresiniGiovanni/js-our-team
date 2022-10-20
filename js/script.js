6ca820f4f8fe27d1d940bc92f75db0aa196c0275
AndresiniGiovanni <andresini.giovanni@gmail.com>
2022-10-20 17:07:34 +0200

    script finito

--- js/script.js
+++ js/script.js
@@ -9,7 +9,83 @@
 // BONUS 1:
 // Trasformare la stringa foto in una immagine effettiva
 // BONUS 2:
-// Organizzare i singoli membri in card/schede e stilarli a vostro gusto :faccia_leggermente_sorridente:
+// Organizzare i singoli membri in card/schede e stilarli a vostro gusto
 // BONUS 3:
 // Aggiungere attraverso un form un membro al team
 
+function team() {
+  const persone = [
+    {
+      nome: "Wayne",
+      cognome: "Barnett",
+      role: "Founder & CEO",
+      image: "wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+      nome: "Angela",
+      cognome: "Caroll",
+      role: "Chief Editor",
+      image: "angela-caroll-chief-editor.jpg",
+    },
+
+    {
+      nome: "Walter",
+      cognome: "Gordon",
+      role: "Office Manager",
+      image: "walter-gordon-office-manager.jpg",
+    },
+
+    {
+      nome: "Angela",
+      cognome: "Lopez",
+      role: "Social Media Manager",
+      image: "angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+      nome: "Scott",
+      cognome: "Estrada",
+      role: "Developer",
+      image: "scott-estrada-developer.jpg",
+    },
+
+    {
+      nome: "Barbara",
+      cognome: "Ramos",
+      role: "Graphic Designer",
+      image: "barbara-ramos-graphic-designer.jpg",
+    },
+  ];
+
+  function print() {
+    for (const persona of persone) {
+      const colPersona = document.createElement("div");
+      colPersona.classList.add("col-4");
+
+      const cardPersona = document.createElement("div");
+      cardPersona.classList.add("card", "mycard");
+
+      const cardImagePersona = document.createElement("img");
+      cardImagePersona.classList.add("card-img-top");
+      cardImagePersona.src = `./img/${persona.image}`;
+
+      const cardBodyPersona = document.createElement("div");
+      cardBodyPersona.classList.add("card-body");
+
+      cardPersona.append(cardImagePersona);
+      cardPersona.append(cardBodyPersona);
+      colPersona.append(cardPersona);
+      document.getElementById("mycontainer").append(colPersona);
+
+      for (const key in persona) {
+        console.log(persona[key]);
+        const childElement = document.createElement("div");
+        childElement.innerHTML = persona[key];
+        cardBodyPersona.append(childElement);
+      }
+    }
+  }
+  print();
+}
+team();
